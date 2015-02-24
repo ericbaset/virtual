083495a8a80b38b8055661bd3b199293f527e6d5
eric <eric@eric-virtual-machine>
2015-02-24 20:27:40 +0100

    Original

---
+++ ED/src/rastreator.java
@@ -0,0 +1,38 @@
+/**
+ * Esta clase hace una tirada de un dado de un tamaño especifico y la 
+ * imprime en pantalla
+ * @author eric
+ *
+ */
+public class rastreator {
+
+	/**
+	 * no requiere de parametros en el constructor
+	 */
+	public rastreator(){
+
+	}
+	/**
+	 * este metodo realiza la tirada de un numero entero y genera otro numero entero
+	 * @param TamañoDado este valor sera el tamaño del dado
+	 * @return devolvera el resultado como numero entero
+	 */
+	public int dados(int TamañoDado){
+		int tirada;
+		tirada=(int)(Math.random()*TamañoDado)+1;
+		return tirada;
+	}
+	/**
+	 * este metodo devuelve una cadena de Strings
+	 */
+	public String toString(){
+		return "Tu tirada es de :";
+	}
+	public static void main(String[] args) {
+		rastreator c = new rastreator();
+		System.out.println(c.toString()+" "+c.dados(20));
+
+
+	}
+
+}
